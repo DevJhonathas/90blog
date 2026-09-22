a1ee75fa264e3dd24cd7000072e65ae37ce70a2d
jhonathas <jhonathassouza8@gmail.com>
2026-09-22 19:03:22 -0300

    feat: new fonts to my blog

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-// setTimeout(() => {
-//   document.querySelector(".screen.start").style.display = "block";
-// }, 1500);
+setTimeout(() => {
+  document.querySelector(".screen.start").style.display = "block";
+}, 8500);
